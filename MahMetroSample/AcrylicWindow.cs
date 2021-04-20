15e65e2394feb3f547f4a4be9f454bf86e62e4bf
Matteo Pagani <matteo.pagani@microsoft.com>
2021-04-20 17:53:47 +0200

    Add project files.

---
+++ MahMetroSample/AcrylicWindow.cs
@@ -0,0 +1,101 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace MahMetroSample
+{
+
+    internal enum AccentState
+    {
+        ACCENT_DISABLED = 1,
+        ACCENT_ENABLE_GRADIENT = 0,
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+        ACCENT_ENABLE_BLURBEHIND = 3,
+        ACCENT_INVALID_STATE = 4
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public int GradientColor;
+        public int AnimationId;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+
+    internal enum WindowCompositionAttribute
+    {
+        // ...
+        WCA_ACCENT_POLICY = 19
+        // ...
+    }
+    public class AcrylicWindow : MetroWindow
+    {
+        public AcrylicWindow()
+        {
+            //InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        public void EnableBlur()
+        {
+
+            var windowHelper = new WindowInteropHelper(this);
+
+            var accent = new AccentPolicy();
+            var accentStructSize = Marshal.SizeOf(accent);
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+            accent.AccentFlags = 2;
+            accent.GradientColor = 0x00FFFFFF;
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        public void DisableBlur()
+        {
+            var windowHelper = new WindowInteropHelper(this);
+
+            var accent = new AccentPolicy();
+            var accentStructSize = Marshal.SizeOf(accent);
+            accent.AccentState = AccentState.ACCENT_ENABLE_GRADIENT;
+            accent.AccentFlags = 2;
+            accent.GradientColor = 0x00FFFFFF;
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+
+    }
+}
---
+++ MahMetroSample/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Bogus;
+using MahApps.Metro.Controls;
+using MahMetroSample.Entities;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MahMetroSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : AcrylicWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void AcrylicWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ringLoading.IsActive = true;
+            await Task.Delay(2000);
+            LoadNotifications();
+            txtComputerName.Text = Environment.MachineName;
+            txtIpAddress.Text = GetLocalIPAddress();
+            txtWindows10Version.Text = Environment.OSVersion.VersionString.Replace("Microsoft Windows NT", string.Empty);
+            ringLoading.IsActive = false;
+        }
+
+        private void LoadNotifications()
+        {
+            var notifications = new Faker<Notification>()
+                .RuleFor(o => o.Title, f => f.Lorem.Text())
+                .RuleFor(o => o.Description, f => f.Lorem.Text())
+                .RuleFor(o => o.Timestamp, f => f.Date.Past())
+                .Generate(2);
+
+            listNotifications.ItemsSource = notifications;
+        }
+
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void toggleBlur_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (toggleBlur.IsOn)
+            {
+                this.EnableBlur();
+            }
+            else
+            {
+                this.DisableBlur();
+            }
+        }
+
+        private void toggleTitleBar_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (toggleTitleBar.IsOn)
+            {
+                this.ShowTitleBar = true;
+            }
+            else
+            {
+                this.ShowTitleBar = false;
+            }
+        }
+
+        private void OnOpenSupportPage(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.onevinn.com/"){ UseShellExecute = true });
+        }
+    }
+}
